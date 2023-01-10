9b81245b57c42836da6180e190b91737fa6c3b19
Marcel Rosier <marcel.rosier@gmx.de>
2023-01-10 19:10:46 +0100

    - addeed spheres code

--- src/harmonic_motion/sketch.js
+++ src/harmonic_motion/sketch.js
@@ -1,75 +1,48 @@
 const CANVAS_SIZE = 800;
-const MAX_HEIGHT = 400;
-const BAR_GROWTH_FACTOR = 1.005;
-const FRAME_RATE = 30;
-const ACCENT_COLOR = (3, 196, 161);
 
-let bar_width = 40;
 let cur_angle = 0;
-let angle_offset = 0.03;
-let max_dist_from_center;
-let is_shrinking = false;
+let angle_offset = 0.01;
 let frames;
-
 function setup() {
-  // noLoop()
-  // frameRate(FRAME_RATE);
-  // smooth();
-  // noLoop()
   createCanvas(800, 800, WEBGL);
-
-  max_dist_from_center = dist(0, 0, CANVAS_SIZE, CANVAS_SIZE);
   frames = TWO_PI / angle_offset;
-  console.log(frames);
 }
 
 function draw() {
   background(0);
-
-  // camera & orientation
-  // ortho(-width, width, height, -height, 0, 2000);
-  ortho(
-    -CANVAS_SIZE + 220,
-    CANVAS_SIZE - 220,
-    CANVAS_SIZE - 220,
-    -CANVAS_SIZE + 220,
-    0,
-    2000
-  );
-  rotateX(PI / 6);
-  rotateY(PI / 8);
-
-  // lighting
-  let light_pos = createVector(1, -1, -1);
-  // ambientLight(60);
+  noStroke();
+  // ambientLight(10);
+  rotateX(cur_angle * 2);
+  rotateY(cur_angle);
+  rotateZ(cur_angle * 2);
+  directionalLight(125, 0, 125, 1, 1, 0);
+  directionalLight(0, 0, 125, cos(cur_angle), -sin(cur_angle), 0);
+  specularMaterial(255, 255, 255);
   push();
-
-  translate(light_pos);
-  box(20);
+  cur_x = map(cos(cur_angle + PI), -1, 1, -200, 200);
+  cur_y = map(sin(cur_angle), -1, 1, -200, 200);
+  translate(cur_x, cur_y, cur_y);
+  sphere(map(sin(cur_angle - PI), -1, 1, 50, 100));
   pop();
-  directionalLight(255, 0, 255, light_pos);
-  // stroke(255);
-  // rotateX(PI / 6 + cur_angle / 10);
-  rotateY(PI / 8 + cur_angle);
-  specularMaterial(3, 196, 161);
-  let bar_spacing = bar_width / 4;
-  for (let z = -width / 2; z < width / 2; z += bar_width) {
-    for (let x = -width / 2; x < width / 2; x += bar_width) {
-      push();
-      let distance = dist(0, 0, x, z);
-      let offset = map(distance, 0, max_dist_from_center, -2 * TWO_PI, TWO_PI);
-      cur_height = -map(sin(cur_angle + offset), -1, 1, 10, MAX_HEIGHT);
-      translate(x, 0, z);
-      box(bar_width - bar_spacing, cur_height, bar_width - bar_spacing); //, cur_height, 10);
-      pop();
-    }
-  }
-  cur_angle -= angle_offset;
-
-  if (is_shrinking) {
-    bar_width /= BAR_GROWTH_FACTOR;
-    is_shrinking = bar_width > 10;
-  }
+  push();
+  cur_x = map(cos(cur_angle), -1, 1, -200, 200);
+  cur_y = map(sin(cur_angle), -1, 1, -200, 200);
+  translate(cur_x, cur_y, 0);
+  sphere(map(sin(cur_angle), -1, 1, 50, 100));
+  pop();
+  push();
+  cur_x = map(cos(cur_angle - HALF_PI), -1, 1, -200, 200);
+  cur_y = map(sin(cur_angle - HALF_PI), -1, 1, -200, 200);
+  translate(cur_x, cur_y, 0);
+  sphere(map(cos(cur_angle), -1, 1, 50, 100));
+  pop();
+  // push();
+  // cur_x = map(cos(cur_angle - HALF_PI), -1, 1, -200, 200);
+  // cur_y = map(sin(cur_angle), -1, 1, -200, 200);
+  // translate(cur_x, cur_y, cur_x);
+  // sphere(100);
+  // pop();
+  cur_angle += angle_offset;
 }
 
 function keyPressed() {
